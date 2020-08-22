2186d133fccb6781d10fa9ffb908daebd8aa0dfe
Leo <leomerel@yahoo.fr>
2020-08-22 16:24:18 +0200

    Improvement by adding navbars

--- site.py
+++ site.py
@@ -5,9 +5,34 @@ spotifyObject = spotifyStatistics.connect()
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/home")
 def home():
-	return render_template("homepage.html", artists=spotifyStatistics.top_artists(spotifyObject, 50, 'long_term'), title="Most played artists of all time")
+	return render_template("homepage.html",
+	 artists=spotifyStatistics.top_artists(spotifyObject, 50, 'long_term'),
+	 title1="Most played artists of all time",
+	 tracks=spotifyStatistics.top_tracks(spotifyObject, 50, 'long_term'),
+	 title2="Most played tracks of all time",
+	 recentTracks=spotifyStatistics.recently_played_tracks(spotifyObject),
+	 title3="Recently played tracks")
+
+@app.route("/topArtists")
+def topArtists():
+	return render_template("topArtists.html",
+	 artists_4weeks=spotifyStatistics.top_artists(spotifyObject, 50, 'short_term'),
+	 artists_6months=spotifyStatistics.top_artists(spotifyObject, 50, 'medium_term'),
+	 artists_alltime=spotifyStatistics.top_artists(spotifyObject, 50, 'long_term'))
+
+@app.route("/topTracks")
+def topTracks():
+	return render_template("topTracks.html",
+	 tracks=spotifyStatistics.top_tracks(spotifyObject, 50, 'long_term'),
+	 title="Most played tracks of all time")
+
+@app.route("/recentlyPlayed")
+def recentlyPlayed():
+	return render_template("recentlyPlayed.html",
+	 recentTracks=spotifyStatistics.recently_played_tracks(spotifyObject),
+	 title="Recently played tracks")
 
 if __name__ == "__main__":
 	app.run()
