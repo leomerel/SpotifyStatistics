e91b00d993059a56f9ad69e26a3a53287acb0f95
Leo <leomerel@yahoo.fr>
2020-08-27 17:27:51 +0200

    Recently played tracks page improved

--- spotifyStatistics.py
+++ spotifyStatistics.py
@@ -5,6 +5,7 @@ import spotipy
 import spotipy.util as util
 from json.decoder import JSONDecodeError
 from auth import startup
+import time
 
 class Artist:
   def __init__(self, name, imageUrl, popularity):
@@ -20,8 +21,16 @@ class Track:
 		self.imageUrl = imageUrl
 		self.popularity = popularity
 
+class RecentTrack(Track):
+    def __init__(self, id, name, artist, imageUrl, popularity, played_at):
+        Track.__init__(self, id, name, artist, imageUrl, popularity)
+        self.played_at = played_at
+
+
+        
+
 def connect():
-	token= startup.getAccessToken() 
+	token= startup.getAccessToken()
 
 	# Create our spotify object with permissions
 	spotifyObject = spotipy.Spotify(auth=token[0])
@@ -42,38 +51,29 @@ def recently_played_tracks(spotifyObject):
 	recently_played = spotifyObject.current_user_recently_played()
 	recently_played = recently_played['items']
 
-	track_list = list()
-	z=1
+	recent_track_list = list()
 	for item in recently_played:
-	    track_list.append(str(z) + '. ' + 
-	    	item['track']['artists'][0]['name'] +
-	    	" - " + item['track']['name'] +
-	    	" (" + item['played_at'] + ')')
-	    z+=1
+		track = spotifyObject.track(item['track']['id'])
+		p = item['played_at']
+		p = p.split("T")
+		played_at = [p[0], p[1][:-5]]
+		recent_track = RecentTrack(item['track']['id'], item['track']['name'], item['track']['artists'][0]['name'], track['album']['images'][0]['url'], track['popularity'], played_at)
+		recent_track_list.append(recent_track)
 
-	return track_list
+	return recent_track_list
 
 
 #Return the most listend artists
 #	limit: from 1 to 50
 #	time_range: short_term (~1 month), medium_term (~6 months), long_term (all time)
 def top_artists(spotifyObject, limit, time_range):
-	if time_range == 'short_term':
-		print('Most played artists for the last  4 weeks: ')
-	elif time_range == 'medium_term':
-		print('Most played artists for the last 6 months: ')
-	elif time_range == 'long_term':
-		print('Most played artists of all time: ')
-
 	top_artists = spotifyObject.current_user_top_artists(limit=limit, offset=0, time_range=time_range)
 	top_artists = top_artists['items']
 
 	artist_list = list()
-	z=1
 	for item in top_artists:
 		artist = Artist(item['name'],item['images'][0]['url'],item['popularity'])
 		artist_list.append(artist)
-		z+=1
 
 	return artist_list
 
@@ -82,21 +82,12 @@ def top_artists(spotifyObject, limit, time_range):
 #	limit: from 1 to 50
 #	time_range: short_term (~1 month), medium_term (~6 months), long_term (all time)
 def top_tracks(spotifyObject, limit, time_range):
-	if time_range == 'short_term':
-		print('Most played tracks for the last  4 weeks: ')
-	elif time_range == 'medium_term':
-		print('Most played tracks for the last 6 months: ')
-	elif time_range == 'long_term':
-		print('Most played tracks of all time: ')
-
 	top_tracks = spotifyObject.current_user_top_tracks(limit=limit, offset=0, time_range=time_range)
 	top_tracks = top_tracks['items']
 
 	track_list = list()
-	z=1
 	for item in top_tracks:
 		track = Track(item['id'], item['name'], item['artists'][0]['name'], item['album']['images'][0]['url'], item['popularity'])
 		track_list.append(track)
-		z+=1
 
 	return track_list
